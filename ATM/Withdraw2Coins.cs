4fc7ae36ec175045a8df126e584dd1e0db5c3309
Jonas Bay Andreasen <au616208@uni.au.dk>
2020-03-27 09:01:22 +0100

    Jonas

---
+++ ATM/Withdraw2Coins.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM
+{
+    public class Withdraw2Coins : IWithdraw
+    {
+        private IWithdraw nexInChain;
+        private int _2coinsCount = 0;
+
+        public void SetNextWithdrawChain(IWithdraw c)
+        {
+            nexInChain = c;
+        }
+
+        public void Withdraw(Amount request)
+        {
+            if (request.amountLeft_ > 2)
+            {
+                while (request.amountLeft_ > 2)
+                {
+                    _2coinsCount++;
+                    request.amountLeft_ -= 2;
+                }
+
+                Console.WriteLine("You have withdrawed " + _2coinsCount + " x 2 coins.");
+                Console.WriteLine("Remaining amount: " + request.amountLeft_);
+
+                nexInChain.Withdraw(request);
+            }
+            else
+                nexInChain.Withdraw(request);
+        }
+    }
+}
